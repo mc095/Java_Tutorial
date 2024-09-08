f920742edef9ad478bdbb358180654a4871c414c
zcat07 <143707195+ganesh9714@users.noreply.github.com>
2024-09-08 23:19:36 +0530

    relationalOperators

---
+++ Operators/relationalOperators.java
@@ -0,0 +1,26 @@
+package Operators;
+
+public class relationalOperators {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 13;
+
+        // is equal to
+        System.out.println(num1 == num2); // false
+
+        // is not eual to
+        System.out.println(num1 != num2); // true
+
+        // is greater than
+        System.out.println(num1 > num2); // false 
+
+        //is less than
+        System.out.println(num1 < num2);// true
+
+        // is greater than or equal to
+        System.out.println(num1 >= num2); // false
+
+        // less than or equal to
+        System.out.println(num1 <= num2); // true
+    }
+}
