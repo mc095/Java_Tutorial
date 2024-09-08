8bb4b0b88b93f3c10ef272a3ab9a279b380676e1
zcat07 <143707195+ganesh9714@users.noreply.github.com>
2024-09-08 23:05:12 +0530

    arithmeticOperators

---
+++ Operators/arithmeticOperators.java
@@ -0,0 +1,24 @@
+package Operators;
+
+public class arithmeticOperators {
+    public static void main(String[] args) {
+        
+        int num1 = 12;
+        int num2 = 6;
+
+        // Addition
+        System.out.println(num1 + num2);
+
+        // Subtraction
+        System.out.println(num1 - num2);
+
+        // Multiplication
+        System.out.println(num1 * num2);
+
+        // Division
+        System.out.println(num1 / num2);
+
+        // Modulo
+        System.out.println(num1 % num2);
+    }
+}
